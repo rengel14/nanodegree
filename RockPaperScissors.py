67d8283bb50cb1e2969305e52ddc6bb68f780bee
rengel14 <46986219+rengel14@users.noreply.github.com>
2019-06-08 19:45:18 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -22,12 +22,7 @@ class Player:
 class RandomPlayer(Player):
     def move(self):
         i = random.randint(0, 2)
-        if(i == 0):
-            return "rock"
-        elif(i == 1):
-            return "scissors"
-        else:
-            return "paper"
+        return moves[i]
 
     def learn(self, my_move, their_move):
         pass
@@ -102,12 +97,32 @@ def update_score(move1, move2, score):
 
 
 class Game:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
+    def __init__(self):
         self.score = [0, 0, 0]
+        self.round = 0
+        self.p1 = HumanPlayer()
+        self.select_opponent()
+
+    def select_opponent(self):
+        opponentInt = input(
+            "Select which version you would like to play against.\n"
+            "1: Random Player\n"
+            "2: Mirroring Player\n"
+            "3: Cycling Player\n"
+            "4: Human Player\n"
+            "5: Surprise Me (Picks betweens options 1-3)\n")
+        while(opponentInt not in ['1', '2', '3', '4', '5']):
+            opponentInt =
+            input("Invalid Input. Please select a number between 1 and 5: ")
+        opponent = [RandomPlayer(), ReflectPlayer(),
+                    CyclePlayer(), HumanPlayer()]
+        if(opponentInt == "5"):
+            self.p2 = opponent[random.randint(0, 2)]
+        else:
+            self.p2 = opponent[int(opponentInt) - 1]
 
     def play_round(self):
+        print(f"Round {self.round + 1}:")
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Player 1: {move1}  Player 2: {move2}")
@@ -115,24 +130,18 @@ class Game:
         self.p2.learn(move2, move1)
         update_score(move1, move2, self.score)
         print(
-            f"Score: " +
-            "Player One: {self.score[0]}, " +
-            "Player Two: {self.score[1]}, " +
-            "Ties: {self.score[2]}\n")
+            "Score: " +
+            f"Player One: {self.score[0]}, " +
+            f"Player Two: {self.score[1]}, " +
+            f"Ties: {self.score[2]}\n")
 
-    def play_game(self):
-        print("Game start!")
-        round = 0
-        while(abs(self.score[0] - self.score[1]) < 3):
-            print(f"Round {round}:")
-            self.play_round()
-            round += 1
+    def end_game(self):
         print("Game over!\n")
         print(
-            f"Final Score\n\t" +
-            "Player One: {self.score[0]}\n\t" +
-            "Player Two: {self.score[1]}\n\t" +
-            "Ties: {self.score[2]}")
+            "Final Score\n\t" +
+            f"Player One: {self.score[0]}\n\t" +
+            f"Player Two: {self.score[1]}\n\t" +
+            f"Ties: {self.score[2]}")
         winner = ""
         if(self.score[0] > self.score[1]):
             print("WINNER: PLAYER ONE!!!!\n")
@@ -141,7 +150,28 @@ class Game:
         else:
             print("TIE GAME!!!!\n")
 
+    def play_game(self):
+        gameType = input("Enter the number of rounds to play, "
+                         "or hit 0 to play until someone is up by 3: ")
+        while(True):
+            try:
+                gameType = int(gameType)
+                break
+            except:
+                gameType = input(
+                    "Invalid response. Please Enter an integer number: ")
+        print("Game start!")
+        if(gameType == 0):
+            while(abs(self.score[0] - self.score[1]) < 3):
+                self.play_round()
+                self.round += 1
+        else:
+            for roundNum in range(gameType):
+                self.play_round()
+                self.round += 1
+        self.end_game()
+
 
 if __name__ == '__main__':
-    game = Game(CyclePlayer(), RandomPlayer())
+    game = Game()
     game.play_game()
